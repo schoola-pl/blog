e2503c1e9945f21fd66d555703c9c09ac0c7191c
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-06-18 11:44:12 +0200

    Add links to articles

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { ArticleBody } from '../../organisms/Articles/Articles.types';
+import { router } from 'next/client';
 
-const Card: React.FC<{ article: ArticleBody }> = ({ article: { title, description, category, thumbnail } }) => {
+const Card: React.FC<{ article: ArticleBody }> = ({ article: { title, slug, description, category, thumbnail } }) => {
   return (
-    <article>
+    <article onClick={() => router.push(`/articles/${slug}`)}>
       <header>
         <img src={thumbnail.url} width={250} height={250} />
         <h1>
