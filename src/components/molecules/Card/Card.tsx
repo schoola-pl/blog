32a29ade56f2cd1ebb1162ef734d054107e770d3
TeodorWolski <teodorwolski88@gmail.com>
2022-06-18 19:12:08 +0200

    accepting changes

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-top: 2rem;
+  width: 80%;
+`;
+
+const CardWrapper = styled.div`
+  height: 5rem;
+  width: 100%;
+  background-color: black;
+  max-width: 24rem;
+`;
 
 const Card = () => {
   return (
-    <div>
-      <h1>Card</h1>
-    </div>
+    <Wrapper>
+      <CardWrapper>
+        <h1>Card</h1>
+      </CardWrapper>
+    </Wrapper>
   );
 };
 
--- src/views/PostsSection/PostsSection.tsx
+++ src/views/PostsSection/PostsSection.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Card from '../../components/molecules/Card/Card';
 
 const PostSection = () => {
