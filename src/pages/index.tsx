130b9e252afb0388fc79008867b78b798ea7bc74
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-06-18 05:48:37 +0200

    Add SSG-oriented data fetching for index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,11 @@ import Head from 'next/head';
 import PageTemplate from '../components/templates/PageTemplate/PageTemplate';
 import FirstSection from '../views/FirstSection/FirstSection';
 import Footer from '../components/organisms/Footer/Footer';
+import Articles from '../components/organisms/Articles/Articles';
+import axios from 'axios';
+import { ArticlesType, CategoriesType } from '../components/organisms/Articles/Articles.types';
 
-const Home: NextPage = () => {
+const Home: NextPage<{ articles: ArticlesType; categories: CategoriesType }> = ({ articles, categories }) => {
   return (
     <div>
       <Head>
@@ -14,10 +17,27 @@ const Home: NextPage = () => {
       </Head>
       <PageTemplate>
         <FirstSection />
+        <Articles articles={articles} categories={categories} />
       </PageTemplate>
       <Footer />
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const {
+    data: { data: articles }
+  } = await axios.get('http://localhost:3000/api/articles');
+  const {
+    data: { data: categories }
+  } = await axios.get('http://localhost:3000/api/categories');
+
+  return {
+    props: {
+      articles,
+      categories
+    }
+  };
 };
 
 export default Home;
