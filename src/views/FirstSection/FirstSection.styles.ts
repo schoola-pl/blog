79648db9181e5225ba8318f8be9ba536f3d779e3
TeodorWolski <teodorwolski88@gmail.com>
2022-06-21 12:31:24 +0200

    RWD for tablets

--- src/views/FirstSection/FirstSection.styles.ts
+++ src/views/FirstSection/FirstSection.styles.ts
@@ -6,6 +6,10 @@ export const Wrapper = styled.div`
   width: 100vw;
   display: flex;
   flex-direction: column;
+
+  @media (min-width: 700px) {
+    height: 70vh;
+  }
 `;
 
 export const HeadingSection = styled.div`
@@ -18,17 +22,34 @@ export const HeadingSection = styled.div`
   align-items: flex-start;
   padding: 2rem;
 
+  @media (min-width: 650px) {
+    transform: translateY(5%);
+  }
+
   h1 {
     color: black;
     font-size: ${({ theme }) => theme.fontSize.xl};
     font-weight: ${({ theme }) => theme.fontWeight.semibold};
+
+    @media (min-width: 440px) {
+      margin-top: 7.5rem;
+    }
+
+    @media (min-width: 600px) {
+      font-size: 5rem;
+      transform: translateY(20%);
+    }
+
+    @media (min-width: 700px) {
+      font-size: ${({ theme }) => theme.fontSize.xxl};
+    }
   }
 
   div {
     width: 100%;
-    display: grid;
-    grid-template-columns: 50% 30%;
+    display: flex;
     align-items: center;
+    justify-content: space-around;
 
     button {
       display: flex;
@@ -38,9 +59,15 @@ export const HeadingSection = styled.div`
       background-color: ${({ theme }) => theme.colors.mainBlue};
       color: white;
       height: 4.75rem;
-      width: 13rem;
+      min-width: 13rem;
       border-radius: 2.5rem;
       margin-left: 5rem;
+
+      @media (min-width: 700px) {
+        height: 5rem;
+        width: 15rem;
+        font-size: ${({ theme }) => theme.fontSize.s};
+      }
     }
 
     p {
@@ -48,6 +75,17 @@ export const HeadingSection = styled.div`
       font-size: ${({ theme }) => theme.fontSize.s};
       color: black;
       z-index: 99999;
+
+      @media (min-width: 440px) {
+        font-size: 1.75rem;
+      }
+
+      @media (min-width: 600px) {
+        font-size: ${({ theme }) => theme.fontSize.m};
+      }
+      @media (min-width: 700px) {
+        font-size: 2.7rem;
+      }
     }
   }
 `;
